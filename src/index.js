384c12c11bbbfa6c17e9ce883cb439ad20769232
chris <mr.chrisnash@gmail.com>
2022-01-03 15:21:38 -0800

    custom middleware

--- src/index.js
+++ src/index.js
@@ -1,26 +1,90 @@
-import 'dotenv/config';
-import cors from 'cors';
-import express from 'express';
+const { v4: uuidv4 } = require('uuid')
+require('dotenv').config()
+const cors = require('cors');
+const express = require('express');
 
 const app = express();
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  return res.send('Received a GET HTTP method');
+// extracts the body of the incoming request and makes it accessible on req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.me = users[1];
+  next();
 });
 
-app.post('/', (req, res) => {
-  return res.send('Received a POST HTTP method');
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
+
+app.get('/session', (req, res) => {
+  return res.send(users[req.me.id]);
 });
 
-app.put('/', (req, res) => {
-  return res.send('Received a PUT HTTP method');
+app.get('/users', (req, res) => {
+  return res.send(Object.values(users));
 });
 
-app.delete('/', (req, res) => {
-  return res.send('Received a DELETE HTTP method');
+app.get('/users/:userId', (req, res) => {
+  return res.send(users[req.params.userId]);
 });
+
+app.get('/messages', (req, res) => {
+  return res.send(Object.values(messages));
+});
+
+app.get('/messages/:messageId', (req, res) => {
+  return res.send(messages[req.params.messageId]);
+});
+
+app.post('/messages', (req, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+    text: req.body.text,  // express.json() allows us to do this
+    userId: req.me.id,
+  };
+
+  messages[id] = message;
+
+  return res.send(message);
+});
+
+app.delete('/messages/:messageId', (req, res) => {
+  const {
+    [req.params.messageId]: message,
+    ...otherMessages
+  } = messages;
+
+  // exclude the message we want to delete from messages object
+  messages = otherMessages;
+
+  return res.send(message);
+});
+
 
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
